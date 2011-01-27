b6e089a0ba6b418a184be9d0e9d160a8cd79a705
Vilhjalmsson <bjarni.vilhjalmsson@Bjarni-Vilhjalmssonss-MacBook-Pro.local>
2011-01-27 22:30:09 +0100

    various stuff

--- variation/src/analyze_snps_data.py
+++ variation/src/analyze_snps_data.py
@@ -348,7 +348,7 @@ def _load_r2_results_(file_prefix='/storage/r2_results/250K_r2_min015'):#_mac15'
 
 
 
-def load_chr_res_dict(r2_thresholds=[(0.8, 25000), (0.6, 50000), (0.4, 100000)], final_r2_thres=0.3):
+def load_chr_res_dict(r2_thresholds=[(0.8, 25000), (0.6, 50000), (0.5, 100000), (0.4, 200000))], final_r2_thres = 0.3):
 	headers = ['x_chr', 'x_pos', 'y_chr', 'y_pos', 'r2', 'pval']#, 'f_stat', 'emmax_pval', 'beta', 'emmax_r2']
 	res_dict = _load_r2_results_()
 	num_res = len(res_dict['x_chr'])
