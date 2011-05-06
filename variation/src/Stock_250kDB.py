c6b12128a1ad6c43b8509d971564e396211490bb
Uemit Seren <uemit.seren@gmi.oeaw.ac.at>
2011-05-06 16:57:28 +0200

    function getSNPChrPos2IDLs added to retrieve both maps

--- variation/src/Stock_250kDB.py
+++ variation/src/Stock_250kDB.py
@@ -2951,6 +2951,33 @@ class Stock_250kDB(ElixirDB):
 					(TableClass.table.name, CNVTableClass.table.name, " and ".join(where_sql_ls))
 		return self.getIDShortNameInfoGivenSQLQuery(sql_string)
 	
+	
+	def getSNPChrPos2IDLs(self,priorTAIRVersion=False):
+		"""
+		2011-5-6
+			returns both maps
+		"""
+		dict_to_return = {}
+		snp_id2chr_pos = {}
+		chr_pos_2snp_id = {}
+		if priorTAIRVersion==True:
+			chromosome_table_fieldname = 'tair8_chromosome'
+			position_table_fieldname = 'tair8_position'
+		else:
+			chromosome_table_fieldname = 'chromosome'
+			position_table_fieldname = 'position'
+		
+		rows = self.metadata.bind.execute("select id, %s as chromosome, %s as position from %s where %s is not null and %s is not null"\
+										%(chromosome_table_fieldname, position_table_fieldname, Snps.table.name, \
+										chromosome_table_fieldname, position_table_fieldname, ))
+		for row in rows:
+			chr_pos_2snp_id[(row.chromosome,row.position)] = row.id
+			snp_id2chr_pos[row.id] = (row.chromosome,row.position)
+		dict_to_return['snp_id2chr_pos'] = snp_id2chr_pos
+		dict_to_return['chr_pos_2snp_id'] = chr_pos_2snp_id
+		return dict_to_return
+	
+	
 	def getSNPChrPos2ID(self, keyType=1, priorTAIRVersion=False):
 		"""
 		2011-5-4
@@ -2975,25 +3002,12 @@ class Stock_250kDB(ElixirDB):
 				2: id is key, (chr, pos) is value.
 		"""
 		sys.stderr.write("Getting a map between Snps.id and (chr,pos), keyType %s ..."%(keyType))
-		dict_to_return = {}
-		if priorTAIRVersion==True:
-			chromosome_table_fieldname = 'tair8_chromosome'
-			position_table_fieldname = 'tair8_position'
-		else:
-			chromosome_table_fieldname = 'chromosome'
-			position_table_fieldname = 'position'
+		data = self.getSNPChrPos2IDLs(priorTAIRVersion)
 		
-		rows = self.metadata.bind.execute("select id, %s as chromosome, %s as position from %s where %s is not null and %s is not null"\
-										%(chromosome_table_fieldname, position_table_fieldname, Snps.table.name, \
-										chromosome_table_fieldname, position_table_fieldname, ))
-		for row in rows:
-			if keyType==1:
-				key = (row.chromosome, row.position)
-				value = row.id
-			else:
-				key = row.id
-				value = (row.chromosome, row.position)
-			dict_to_return[key] = value
+		if keyType==1:
+			dict_to_return = data.get('chr_pos_2snp_id')
+		else:
+			dict_to_return = data.get('snp_id2chr_pos')
 		sys.stderr.write("%s entries. Done.\n"%(len(dict_to_return)))
 		return dict_to_return
 	
@@ -3058,6 +3072,7 @@ class Stock_250kDB(ElixirDB):
 				chr_pos = db.snp_id2chr_pos.get(1)
 		"""
 		self._snp_id2chr_pos = self.getSNPChrPos2ID(keyType=2, priorTAIRVersion=priorTAIRVersion)
+	
 	
 	@property
 	def chr_pos2snp_id(self,):
