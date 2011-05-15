3e885fdd927f9f91d0f75ee3ae3115640cbf8e1b
Vilhjalmsson <bjarni.vilhjalmsson@gmi-lws04>
2011-05-15 19:23:40 +0200

    working from home

--- variation/src/snpsdata.py
+++ variation/src/snpsdata.py
@@ -3835,7 +3835,11 @@ def test_ibd_kinship():
 
 def _merge_imputed_and_250K_data_():
 	import  dataParsers as dp
+	import tair_converter as tc
 	sd_54 = dp.load_snps_call_method(54, 'binary')
+	print 'Converting to TAIR 9'
+	for snpsd in sd_54.snpsDataList:
+		snpsd.positions = tc.get_tair9_positions(snpsd.positions)
 	sd_76 = dp.load_snps_call_method(76, 'binary')
 	sd_54.merge_snps_data(sd_76)
 	sd_54.writeToFile('/tmp/test_merged_data.csv')
