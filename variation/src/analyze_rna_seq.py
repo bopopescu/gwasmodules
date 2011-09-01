fbe71a5469aede01aa9b61d20da224eb681d2a74
Bjarni Vilhjalmsson <bjarni.vilhjalmsson@gmail.com>
2011-09-01 23:45:44 +0200

    bla

--- variation/src/analyze_rna_seq.py
+++ variation/src/analyze_rna_seq.py
@@ -286,7 +286,7 @@ def plot(temperature=10, call_method_id=75, mapping_method='EX', mac_threshold=1
 		near_const_filter=20, data_format='binary', plot_data=True):
 	#Load in chromosome dict..
 
-	file_prefix = '/srv/lab/data/rna_seq_083011/%dC/cm_%d/' % (temperature, call_method_id)
+	file_prefix = '/srv/lab/data/rna_seq_062911/%dC/cm_%d/' % (temperature, call_method_id)
 
 	results_dict_file = '%sresults_%s_mac%d.pickled' % (file_prefix, mapping_method, mac_threshold)
 	res_dict = cPickle.load(open(results_dict_file))
@@ -436,7 +436,9 @@ def load_and_plot_info_files(call_method_id=75, temperature=10, mac_threshold=15
 
 	print 'Loading the gene annotation dictionary'
 	gene_dict = dp.parse_tair_gff_file()
-	run_id = 'd081511'#_%d' % call_method_id
+	run_id = 'd081511'
+	#run_id = 'rs_%d' % call_method_id
+
 
 	file_prefix = '/srv/lab/data/rna_seq_083011/%dC/cm_%d/' % (temperature, call_method_id)
 
@@ -898,16 +900,16 @@ if __name__ == '__main__':
 #		_test_parallel_()
 #	sys.exit(0)
 #	_test_()
-	load_and_plot_info_files(temperature=10, call_method_id=79, debug_filter=0.005)
-#	plot(min_score=1, temperature=10, mapping_method='KW', call_method_id=79, plot_data=False)
+#	load_and_plot_info_files(temperature=10, call_method_id=79, debug_filter=0.005)
+	plot(min_score=1, temperature=16, mapping_method='KW', call_method_id=79, plot_data=False)
 #	plot(min_score=7, temperature=10, mapping_method='KW')
 #	plot(min_score=10, temperature=16, mapping_method='KW')
 	#plot(min_score=11, temperature=16, mapping_method='KW')
-#	plot(min_score=1, temperature=10, mapping_method='LM', call_method_id=79, plot_data=False)
+	plot(min_score=1, temperature=16, mapping_method='LM', call_method_id=79, plot_data=False)
 #	plot(min_score=7, temperature=16, mapping_method='LM')
 #	plot(min_score=10, temperature=16, mapping_method='LM')
 #	plot(min_score=11, temperature=16, mapping_method='LM')
 #	plot(min_score=3, temperature=10, mapping_method='EX', plot_data=False)
-#	plot(min_score=1, temperature=10, mapping_method='EX', call_method_id=79, plot_data=False)
+	plot(min_score=1, temperature=16, mapping_method='EX', call_method_id=79, plot_data=False)
 	print  'Done'
 
