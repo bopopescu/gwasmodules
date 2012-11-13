f12388a0e20a3a54583735265f91ff5810020780
crocea <crocea@uclaOffice>
2012-11-12 16:27:29 -0800

    expose analysis_method_id_ls to choose association results from these methods and separate analysis methods into EMMA method set and KW (uncorrected) method set

--- variation/src/db/ResultPeak2AssociationLocusStat.py
+++ variation/src/db/ResultPeak2AssociationLocusStat.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 """
 Examples:
-	%s 
-	
-	%s  -i ... -l 32,80,57,75 -u yh -z banyan -s 2,3,1,3
+	#2012.9.30
+	%s  -i 1-35,39-48,57-82,158-159,161-179,182-186,272-283,314-351,362-380,418-589 -l 80,75 -u yh -z banyan -s 2,3,1,3 -c
+	-o ~/script/variation/data/AssociationPeakStat.tsv
+
+	#2012.9.25
+	%s  -i 1-35,39-48,57-82,158-159,161-179,182-186,272-274,277-283,314-351,362-380 -l 32,80,57,75 -u yh -z banyan -s 2,3,1,3 -c
 
 Description:
 	2012.6.1
@@ -24,6 +27,9 @@ from pymodule import SNPData, SNP
 from variation.src.mapper.AbstractVariationMapper import AbstractVariationMapper
 from variation.src import Stock_250kDB
 import networkx as nx
+from pymodule.CNV import CNVCompare, CNVSegmentBinarySearchTreeKey, get_overlap_ratio
+from pymodule.RBTree import RBDict
+import numpy
 
 class ResultPeak2AssociationLocusStat(AbstractVariationMapper):
 	__doc__ = __doc__
@@ -31,7 +37,8 @@ class ResultPeak2AssociationLocusStat(AbstractVariationMapper):
 	option_default_dict.update({
 							('phenotype_id_ls', 1, ): ['', 'i', 1, 'loci from these peaks will be outputted', ],\
 							('call_method_id_ls', 1, ): ['', 'l', 1, 'loci from these peaks will be outputted', ],\
-							('min_overlap_ratio', 1, float): ['0.1', 'm', 1, 'minimum overlap ratio, overlap length/total' ],\
+							('analysis_method_id_ls', 1, ): ['1,32', 'a', 1, 'loci from these peaks will be outputted', ],\
+							('min_overlap_ratio', 1, float): ['0.05', 'm', 1, 'minimum overlap ratio, overlap length/total' ],\
 							('result_peak_type_id_ls', 1, ): ['', 's', 1, 'list of result peak type IDs, in this order:\
 								snp-KW-gwas,snp-EMMA-gwas,deletion-KW-gwas,deletion-EMMA-gwas', ],\
 							})
@@ -42,6 +49,9 @@ class ResultPeak2AssociationLocusStat(AbstractVariationMapper):
 		self.phenotype_id_ls = getListOutOfStr(self.phenotype_id_ls, data_type=int)
 		self.phenotype_id_ls.sort()
 		self.result_peak_type_id_ls = getListOutOfStr(self.result_peak_type_id_ls, data_type=int)
+		self.call_method_id_ls = getListOutOfStr(self.call_method_id_ls, data_type=int)
+		self.analysis_method_id_ls = getListOutOfStr(self.analysis_method_id_ls, data_type=int)
+		
 	
 	def constructGraph(self, resultPeakRBDictList=[], min_overlap_ratio=0.1):
 		"""
@@ -50,6 +60,7 @@ class ResultPeak2AssociationLocusStat(AbstractVariationMapper):
 		sys.stderr.write("Constructing graph between all result peaks ...")
 		rpg = resultPeakGraph = nx.Graph()
 		n = len(resultPeakRBDictList)
+		counter = 0
 		for i in xrange(n):
 			for j in xrange(i+1, n):
 				peakRBDict1 = resultPeakRBDictList[i]
@@ -64,12 +75,12 @@ class ResultPeak2AssociationLocusStat(AbstractVariationMapper):
 					node_ls = []
 					peakRBDict2.findNodes(segmentKey, node_ls=node_ls, compareIns=compareIns)
 					total_perc_overlapped_by_result2 = 0.
-					rpg.add_node(node.result_peak_id, span=segmentKey.span_ls)
+					rpg.add_node(node.key.result_peak_id, span=segmentKey.span_ls)
 					for result2_node in node_ls:
-						if result2_node.result_peak_id not in rpg:
-							rpg.add_node(result2_node.result_peak_id, span = [result2_node.key.start, result2_node.key.stop])
+						if result2_node.key.result_peak_id not in rpg:
+							rpg.add_node(result2_node.key.result_peak_id, span = [result2_node.key.start, result2_node.key.stop])
 						
-						rpg.add_edge(node.result_peak_id, result2_node.result_peak_id)
+						rpg.add_edge(node.key.result_peak_id, result2_node.key.result_peak_id)
 		sys.stderr.write("%s nodes. %s edges. %s connected components.\n"%(rpg.number_of_nodes(), rpg.number_of_edges(), \
 															nx.number_connected_components(rpg) ) )
 		return rpg
@@ -83,9 +94,12 @@ class ResultPeak2AssociationLocusStat(AbstractVariationMapper):
 		counter = 0
 		for cc_graph in cc_graph_list:
 			#calculate connectivity of this component
-			ne = cc_graph.number_of_edges
-			nn = cc_graph.number_of_nodes
-			connectivity = ne/(nn*(nn-1)/2)
+			ne = cc_graph.number_of_edges()
+			nn = cc_graph.number_of_nodes()
+			if nn>1:
+				connectivity = ne/float(nn*(nn-1)/2)
+			else:
+				connectivity = 1
 			start_ls = []
 			stop_ls = []
 			result_peak_ls = []
@@ -119,23 +133,25 @@ class ResultPeak2AssociationLocusStat(AbstractVariationMapper):
 		
 		db_250k.session.begin()
 		
-		result_ls = db_250k.getResultLs(call_method_id=None, analysis_method_id_ls=[1,32], \
+		result_ls = db_250k.getResultLs(call_method_id=None, analysis_method_id_ls=self.analysis_method_id_ls, \
 								phenotype_method_id_ls=self.phenotype_id_ls, \
 								call_method_id_ls=self.call_method_id_ls, cnv_method_id=None)
 		
 		
 		
 		resultPeakRBDictList =[]
+		
+		noCorrectionAnalysisMethodIDSet = set([1,2,3,5,6,16])
+		EMMAAnalysisMethodIDSet = set([4,7,8,32,33])
+		
 		for result in result_ls:
-			if result.call_method.locus_type_id ==1 and result.analysis_method_id==1:	#KW on snp
+			if result.call_method.locus_type_id ==1 and result.analysis_method_id in noCorrectionAnalysisMethodIDSet:	#KW on snp
 				result_peak_type_id = self.result_peak_type_id_ls[0]
-			elif result.call_method.locus_type_id==1 and \
-					(result.analysis_method_id==32 or result.analysis_method_id==8):	#EMMA on snp
+			elif result.call_method.locus_type_id==1 and result.analysis_method_id in EMMAAnalysisMethodIDSet:	#EMMA/EMMAX on snp
 				result_peak_type_id = self.result_peak_type_id_ls[1]
-			elif result.call_method.locus_type_id==2 and result.analysis_method_id==1:	#deletion
+			elif result.call_method.locus_type_id==2 and result.analysis_method_id in noCorrectionAnalysisMethodIDSet:	#KW on deletion
 				result_peak_type_id = self.result_peak_type_id_ls[2]
-			elif result.call_method.locus_type_id==2 and \
-					(result.analysis_method_id==32 or result.analysis_method_id==8):	#EMMA on deletion 
+			elif result.call_method.locus_type_id==2 and result.analysis_method_id in EMMAAnalysisMethodIDSet:	#EMMA on deletion 
 				result_peak_type_id = self.result_peak_type_id_ls[3]
 			else:
 				sys.stderr.write("Error: ")
