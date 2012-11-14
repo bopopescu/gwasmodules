d6496b00d8a86d99651e1c66e002ca053a65f7e4
crocea <crocea@uclaOffice>
2012-11-14 10:08:01 -0800

    change argument results_directory to data_dir added connectDB()

--- variation/src/AbstractVariationWorkflow.py
+++ variation/src/AbstractVariationWorkflow.py
@@ -11,7 +11,7 @@ sys.path.insert(0, os.path.join(os.path.expanduser('~/script')))
 from pymodule import ProcessOptions, getListOutOfStr, PassingData, yh_pegasus, NextGenSeq
 from Pegasus.DAX3 import *
 from pymodule.pegasus.AbstractWorkflow import AbstractWorkflow
-
+import Stock_250kDB
 
 class AbstractVariationWorkflow(AbstractWorkflow):
 	__doc__ = __doc__
@@ -27,7 +27,7 @@ class AbstractVariationWorkflow(AbstractWorkflow):
 						('db_passwd', 1, ): [None, 'p', 1, 'database password', ],\
 						('port', 0, ):[None, '', 1, 'database port number'],\
 						
-						('results_directory', 0, ):["", 't', 1, 'The results directory. Default is None. use the one given by db.'],\
+						('data_dir', 0, ):[None, 't', 1, 'The top folder of db-affiliated storage. If not given, use db.data_dir.'],\
 						
 						('tabixPath', 1, ): ["%s/bin/tabix", '', 1, 'path to the tabix binary', ],\
 						
@@ -47,6 +47,22 @@ class AbstractVariationWorkflow(AbstractWorkflow):
 		import re
 		self.chr_pattern = re.compile(r'(\w+\d+).*')
 		self.contig_id_pattern = re.compile(r'Contig(\d+).*')
+		
+		self.connectDB()
+		
+		if not self.data_dir:
+			self.data_dir = self.db.data_dir
+	
+	def connectDB(self):
+		"""
+		2012.6.5
+			overwrite the parent class
+		"""
+		self.db = Stock_250kDB.Stock_250kDB(drivername=self.drivername, username=self.db_user,
+				password=self.db_passwd, hostname=self.hostname, database=self.dbname, schema=self.schema,\
+				port=self.port)
+		self.db.setup(create_tables=False)
+	
 	
 	def registerExecutables(self, workflow=None):
 		"""
